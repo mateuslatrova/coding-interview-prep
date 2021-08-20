fd4bae3e7a5e421b4acbf41b3fa0177d97810c4b
mateuslatrova <mateusls@ufmg.br>
2021-08-20 13:46:59 -0300

    frequency queries problem solved.

---
+++ hackerRank/interview prep kit/hash tables/freqQueries.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the freqQuery function below.
+vector<int> freqQuery(vector<vector<int>> queries) {
+    vector<int> ans;
+    map<int,int> numToFreq; // maps integer to frequency.
+    map<int,set<int>> freqToNums; // maps integer to frequency.
+    int n = queries.size();
+
+    for (int i = 0; i < n; i++) {
+        int command = queries[i][0];
+        int elem = queries[i][1];
+
+        switch(command) {
+            case 1:
+                if (numToFreq.find(elem) != numToFreq.end()) {
+                    freqToNums[numToFreq[elem]].erase(elem);
+                    numToFreq[elem]++;
+                    freqToNums[numToFreq[elem]].insert(elem);
+                } else {
+                    numToFreq.insert(make_pair(elem,1));
+                    freqToNums[1].insert(elem);
+                }
+                break;
+            case 2:
+                if (numToFreq.find(elem) != numToFreq.end()){
+                    freqToNums[numToFreq[elem]].erase(elem);
+                    numToFreq[elem]--;
+                    if (numToFreq[elem] == 0) {
+                        numToFreq.erase(elem);
+                    } else freqToNums[numToFreq[elem]].insert(elem);
+                }
+                break;
+            case 3:
+                if (freqToNums[elem].size() != 0) ans.push_back(1);
+                else ans.push_back(0);
+                break;
+        }
+    }
+
+    return ans;
+}
+
+// Testing:
+int main () {
+
+    int q;
+    cin >> q;
+    vector<vector<int>> queries;
+
+    while (q--) {
+        int q0, q1;
+        cin >> q0 >> q1;
+        queries.push_back({q0,q1});
+    }
+
+    vector<int> ans = freqQuery(queries);
+
+    for (int x: ans) cout << x << endl;
+}
